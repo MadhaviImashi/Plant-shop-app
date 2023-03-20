aac99aedb9711a16e30be256710d79600ba4c43e
MadhaviImashi <madhaviimashi@gmail.com>
2023-03-20 11:27:50 +0530

    My account page

---
+++ lib/screens/profile/components/body.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'profile_menu.dart';
+import 'profile_pic.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: [
+          ProfilePic(),
+          SizedBox(height: 20),
+          ProfileMenu(
+            text: "My Account",
+            icon: "assets/icons/User Icon.svg",
+            press: () => {Navigator.pushNamed(context, '/profile-update')},
+          ),
+          ProfileMenu(
+            text: "Help Center",
+            icon: "assets/icons/Question mark.svg",
+            press: () {},
+          ),
+          ProfileMenu(
+            text: "Log Out",
+            icon: "assets/icons/Log out.svg",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/profile/components/profile_pic.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 115,
+      width: 115,
+      child: Stack(
+        fit: StackFit.expand,
+        clipBehavior: Clip.none,
+        children: const [
+          CircleAvatar(
+            backgroundImage: AssetImage("assets/images/Profile Image.jpg"),
+          ),
+          Positioned(
+            right: -16,
+            bottom: 0,
+            child: SizedBox(
+              height: 46,
+              width: 46,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
